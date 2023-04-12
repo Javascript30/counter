1ee5b295de01ac9181bbaa86aa8c163b6e7a3e55
Lesley <leskimuti@gmail.com>
2023-04-12 18:07:30 +0300

    Initial file commiit -no styling

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [count, setCount] = useState(localStorage.getItem("yourCount") || 0);
+
+  useEffect(() => {
+    localStorage.setItem("yourCount", count);
+  }, [count]);
+
+  return (
+    <main className="App">
+      <h1>Clicks {count}</h1>
+      <button onClick={() => setCount((count) => JSON.parse(count) + 1)}>
+        Click Here
+      </button>
+    </main>
+  );
+}
+
+export default App;
